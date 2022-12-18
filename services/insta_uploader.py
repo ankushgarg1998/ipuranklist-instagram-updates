5e35d8a21c7c30002c65d89e0803c86dd3ad8988
ankushgarg1998 <1998anky@gmail.com>
2022-12-18 20:33:00 +0530

    Removing logging

--- services/insta_uploader.py
+++ services/insta_uploader.py
@@ -11,8 +11,6 @@ from instabot import Bot
 
 def upload_post(self):
     bot = Bot()
-    print(self.insta_username)
-    print(self.insta_password)
     bot.login(username = self.insta_username, password = self.insta_password)
     bot.upload_photo(config.FINISHED_IMG_PATH, self.caption)
     self.logger.info("Img Upload Success")
